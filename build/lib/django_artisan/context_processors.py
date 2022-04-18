e7ab665792785a3699f6b8e3dbd8bb5a0eaf21bf
James Miller <jamesstewartmiller@gmail.com>
2022-04-18 19:22:32 +0100

    ran setup.py

--- build/lib/django_artisan/context_processors.py
+++ build/lib/django_artisan/context_processors.py
@@ -6,7 +6,6 @@ from django import http
 def navbarSpiel(request: http.HttpRequest) -> Dict[str, Any]:
     return {'navbarSpiel': conf.settings.NAVBAR_SPIEL}
 
-
 def siteLogo(request: http.HttpRequest) -> Dict[str, Any]:
     return {'siteLogo': conf.settings.SITE_LOGO}
 
@@ -21,4 +20,13 @@ def location_visible(request):
 
 def max_images(request):
     return {'MAX_IMAGES': conf.settings.MAX_USER_IMAGES}
+
+def image_exts(request):
+    ae = conf.settings.ALLOWED_EXTENSIONS
+    return {'ALLOWED_EXTENSIONS': ' '.join(
+        map(lambda x: x+',' if x not in (ae[-1],ae[-2]) else x if x is ae[-2] else 'or '+x, ae)
+    )}
+
+def max_upload_size(request):
+    return {'MAX_UPLOAD_SIZE': conf.settings.MAX_UPLOAD_SIZE}
 
